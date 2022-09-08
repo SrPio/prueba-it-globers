744fc02045fb223e49f831a6d4f946762df1e2c6
SrPio <andresfer1799@gmail.com>
2022-09-07 22:02:08 -0500

    Impl. API formspree para la gestión del formulario - Se implementó el llamado a la API de formspree para controlar el formulario de Newsletter. - Cambios visuales en el form.

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "./Newsletter.module.css";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Newsletter() {
+  const [state, handleSubmit] = useForm("xaykqdlb");
+  if (state.succeeded) {
+    return (
+      <div className={styles.container_alerta_subs}>
+        <h2 className={styles.alerta_subs}>
+          Tu correo ha sido registrado con éxito. ¡Gracias por suscribirte!
+        </h2>
+      </div>
+    );
+  }
   return (
     <div className={styles.newsletter}>
       <div className={styles.container_titles}>
@@ -10,10 +21,24 @@ function Newsletter() {
         <h2>Y enterarte de todas las novedades</h2>
       </div>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.conatiner_input}>
-            <input type="email" placeholder="Ingresa tu email" />
-            <button className={styles.btn_submit}>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Ingresa tu email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <button
+              className={styles.btn_submit}
+              type="submit"
+              disabled={state.submitting}
+            >
               <img src="/assets/right-arrow.svg" alt="" />
             </button>
           </div>
